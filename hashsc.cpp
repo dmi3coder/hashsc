0c195f2ec9dd1a7c32a2fd9ddac012cd7dd1d7cf
Dmitry Chaban <dmitrychaban@yahoo.com>
2018-02-12 17:54:13 +0200

    Added file append option

--- hashsc.cpp
+++ hashsc.cpp
@@ -2,8 +2,12 @@
 #include "base64.h"
 #include <string>
 #include "yaml-cpp/yaml.h"
+#include <fstream>
 
 using namespace std;
+
+bool isValid(YAML::Node& config);
+
 int handleCommand(int argc, char* argv[]) {
     cout << argv[1] << endl;
     string argument = argv[1];
@@ -11,7 +15,22 @@ int handleCommand(int argc, char* argv[]) {
         std::cout << "parsing..." << std::endl;
         YAML::Node config = YAML::LoadFile(argv[2]);
         cout << config["apiVersion"].as<string>() << endl;
+        if(isValid(config)) {
+            YAML::Node data = config["data"];
+            string encodeText = argv[4];
+            data[argv[3]] = base64_encode(reinterpret_cast<const unsigned char*>(encodeText.c_str()), encodeText.length());
+            string filepath = argv[2];
+            std::ofstream fout(filepath);
+            fout << config;
+            cout << "OK" << endl;
+        } else {
+            cout << "Sorry, something wrong with this file" << endl;
+        }
     }
+}
+
+bool isValid(YAML::Node &config) {
+    return config["kind"].as<string>() == "Secret";
 }
 
 int main(int argc, char* argv[]) {
@@ -22,7 +41,7 @@ int main(int argc, char* argv[]) {
         std::cout << base64_encode(reinterpret_cast<const unsigned char*>(encodeText.c_str()), encodeText.length()) << std::endl;
     }
         break;
-    case 3:{
+    case 5:{
         return handleCommand(argc, argv);
     }
         break;
