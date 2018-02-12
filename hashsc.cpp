5338c6a09b0a058b4da33455f3b61a7c8cede441
Dmitry Chaban <dmitrychaban@yahoo.com>
2018-02-12 15:22:50 +0200

    Added CMakeLists.txt Linked yaml-cpp project

--- hashsc.cpp
+++ hashsc.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
 #include "base64.h"
-#include <string> 
+#include <string>
+#include "yaml-cpp/yaml.h"
+#include <fstream>
 
 int main(int argc, char* argv[]) {
-    std::string encodeText = argv[1];
-    std::cout << base64_encode(reinterpret_cast<const unsigned char*>(encodeText.c_str()), encodeText.length()) << std::endl;
+    YAML::Node config = YAML::LoadFile("config.yaml");
+    switch (argc) {
+    case 2:{
+        std::string encodeText = argv[1];
+        std::cout << base64_encode(reinterpret_cast<const unsigned char*>(encodeText.c_str()), encodeText.length()) << std::endl;
+    }
+        break;
+    case 3:{
+    }
+        break;
+    default:
+        break;
+    }
     return 0;
 }
