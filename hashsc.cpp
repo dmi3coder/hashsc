4cb34ba708d768994873f491571b34b4e756070f
Dmitry Chaban <dmitrychaban@yahoo.com>
2018-02-12 15:47:25 +0200

    Handling 3 arguments as a yaml file raw

--- hashsc.cpp
+++ hashsc.cpp
@@ -2,10 +2,20 @@
 #include "base64.h"
 #include <string>
 #include "yaml-cpp/yaml.h"
-#include <fstream>
+
+using namespace std;
+int handleCommand(int argc, char* argv[]) {
+    cout << argv[1] << endl;
+    string argument = argv[1];
+    if(argument == "--append") {
+        std::cout << "parsing..." << std::endl;
+        YAML::Node config = YAML::LoadFile(argv[2]);
+        cout << config["apiVersion"].as<string>() << endl;
+    }
+}
 
 int main(int argc, char* argv[]) {
-    YAML::Node config = YAML::LoadFile("config.yaml");
+    std::cout << "Handling "<< argc <<" arguments" << std::endl;
     switch (argc) {
     case 2:{
         std::string encodeText = argv[1];
@@ -13,6 +23,7 @@ int main(int argc, char* argv[]) {
     }
         break;
     case 3:{
+        return handleCommand(argc, argv);
     }
         break;
     default:
