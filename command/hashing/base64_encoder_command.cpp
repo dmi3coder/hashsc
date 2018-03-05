c680a3c34e6f7f2f1460005a50e1927d05d48055
Dmitry Chaban <dmitrychaban@yahoo.com>
2018-03-05 08:55:02 +0200

    removing string after usage in base64_encoder_command

--- command/hashing/base64_encoder_command.cpp
+++ command/hashing/base64_encoder_command.cpp
@@ -7,6 +7,7 @@ int base64_encoder_command::Execute() {
     std:: cout << "base64_encoder_command::Execute()" << std::endl;
     std::string *data = new std::string(std::istreambuf_iterator<char>(*input), {});
     (*output) << base64_encode(reinterpret_cast<const unsigned char*>(data->c_str()), data->length()) << std::endl;
+    delete data;
     return 0;
 }
 
