9670ce5155a5b879a238806c2c118c25d8a710d0
Dmitry Chaban <dmitrychaban@yahoo.com>
2018-03-06 07:51:42 +0200

    switched logging to glog in base64_encoder_command

--- command/hashing/base64_encoder_command.cpp
+++ command/hashing/base64_encoder_command.cpp
@@ -1,10 +1,11 @@
 #include "base64_encoder_command.h"
 #include <sstream>
 #include "base64.h"
+#include <glog/logging.h>
 
 
 int base64_encoder_command::Execute() {
-    std:: cout << "base64_encoder_command::Execute()" << std::endl;
+    DLOG(INFO) << "base64_encoder_command::Execute()" << std::endl;
     std::string *data = new std::string(std::istreambuf_iterator<char>(*input), {});
     (*output) << base64_encode(reinterpret_cast<const unsigned char*>(data->c_str()), data->length()) << std::endl;
     delete data;
