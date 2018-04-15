11aa8be6cbaaa96f2bc2d04dcbea614d24297007
Dmitry Chaban <dmitrychaban@yahoo.com>
2018-04-15 12:25:37 +0300

    Pipe reading functionality raw

--- hashsc.go
+++ hashsc.go
@@ -2,13 +2,22 @@ package main
 
 import (
 	"./handler/parser"
+	"./command/hashing"
+	"./command"
 	"encoding/base64"
+	"os"
+	"bufio"
 )
 
 func main() {
 	parser.Parse()
 	if parser.FlagCount == 0 {
-		//TODO: handle incoming stream
+		var cmd command.Command = hashing.Base64_encoder_command{}
+		if cmd_stream := cmd.(command.Stream_command); cmd_stream != nil {
+			cmd_stream.SetReader(*bufio.NewReader(os.Stdin))
+			cmd_stream.SetWriter(*bufio.NewWriter(os.Stdout))
+		}
+		cmd.Execute()
 	} else {
 		response := base64.StdEncoding.EncodeToString([]byte(parser.Arguments[0]))
 		println(response)
