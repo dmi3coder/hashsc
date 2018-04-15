7a5663718ff1ca75a4ada36d070e161d99991bd2
Dmitry Chaban <dmitrychaban@yahoo.com>
2018-04-15 12:24:08 +0300

    Exposed command and stream_command to global

--- command/command.go
+++ command/command.go
@@ -4,6 +4,6 @@ const CMD_CONVERT_STREAM = "CMD_CONVERT_STREAM"
 const CMD_ENCODE = "CMD_ENCODE"
 const CMD_MULTILINE_ENCODE = "CMD_MULTILINE_ENCODE"
 
-type command interface {
+type Command interface {
 	Execute() int
 }
--- command/stream_command.go
+++ command/stream_command.go
@@ -1,9 +1,10 @@
 package command
 
-import "io"
-
-type stream_command interface {
-	command
-	setReader(reader io.Reader)
-	setWriter(writer io.Writer)
+import (
+	"bufio"
+)
+type Stream_command interface {
+	Command
+	SetReader(reader bufio.Reader)
+	SetWriter(writer bufio.Writer)
 }
