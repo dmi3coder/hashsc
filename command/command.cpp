c76ea1c2c344a87ad6c715cfb4f0bd1012cd0b49
Dmitry Chaban <dmitrychaban@yahoo.com>
2018-04-15 11:43:07 +0300

    Removed cpp command classes

--- command/command.cpp
+++
@@ -1,5 +0,0 @@
-#include "command.h"
-
-const std::string command::CMD_CONVERT_STREAM = "convert_stream";
-const std::string command::CMD_ENCODE = "encode";
-const std::string command::CMD_MULTILINE_ENCODE = "multiline_encode";
